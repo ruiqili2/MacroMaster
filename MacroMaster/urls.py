85337f8346e7d45bd3d2bc18233d43d4e7ddc0b3
Wang <zwang199@fa17-cs411-08.cs.illinois.edu>
2017-10-12 17:04:16 -0500

    aaa

--- MacroMaster/urls.py
+++ MacroMaster/urls.py
@@ -21,7 +21,7 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^$', include('polls.urls')),
-    url(r'^admin/', include('admin.site.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 ]
 
 
