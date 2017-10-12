d5b0938a19598634ab0d4a95ed3a462172b6224c
Li <ruiqili2@fa17-cs411-08.cs.illinois.edu>
2017-10-12 12:43:57 -0500

    switch branch

--- MacroMaster/settings.py
+++ MacroMaster/settings.py
@@ -123,7 +123,8 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+#https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
+STATICFILES_DIRS = (
+	"/home/ruiqili2/cs411-project/MacroMaster/static",
+)
--- MacroMaster/urls.py
+++ MacroMaster/urls.py
@@ -13,11 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-<<<<<<< HEAD:MasterMacro/MasterMacro/urls.py
-from django.conf.urls import include, url
-=======
 from django.conf.urls import *
->>>>>>> 21ade5d02e55fe8cccf5def1405e3af15225fbde:MacroMaster/urls.py
 from django.contrib import admin
 from django.contrib.auth.views import login, logout
 from users.views import SignUpView
@@ -25,10 +21,6 @@ from polls.views import search
 admin.autodiscover()
 
 urlpatterns = [
-<<<<<<< HEAD:MasterMacro/MasterMacro/urls.py
-    url(r'^mmapp/', include('mmapp.urls')),
-    url(r'^admin/', admin.site.urls),
-=======
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
@@ -38,5 +30,4 @@ urlpatterns += [
     url(r'^login/$', login, name='user_login'),
     url(r'^logout/$', logout, name='user_logout'), 
     url(r'^signup$', SignUpView.as_view(), name='user_signup'), 
->>>>>>> 21ade5d02e55fe8cccf5def1405e3af15225fbde:MacroMaster/urls.py
 ]
