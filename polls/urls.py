b8e46639b5a88a5a183da14d2af371b2ae48f138
zwang199 <zwang199@illinois.edu>
2017-10-12 17:00:03 -0500

    urls changes3

--- polls/urls.py
+++ polls/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     url(r'^home/', views.home, name='home'),
     url(r'^search_for/', views.search_page, name='explore'),
     url(r'^add/', views.enter, name='add something')
-    url(r'^pour/', views.pour, )
+    url(r'^pour/', views.pour)
     url(r'^result$', views.search)
 ]
