8addfc62c5983750146302128da0dd0eed595d6a
ruiqili2 <ruiqili2@illinois.edu>
2017-10-20 12:17:43 -0500

    deploy

---
+++ deploy.py
@@ -0,0 +1,49 @@
+import os
+import subprocess
+import shlex
+import signal
+import sys
+proc1 = subprocess.Popen(shlex.split('ps aux'), stdout = subprocess.PIPE)
+proc2 = subprocess.Popen(shlex.split('grep main.py'), stdin = proc1.stdout,
+                         stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+proc3 = subprocess.Popen(shlex.split('grep 8000'), stdin = proc2.stdout, 
+			 stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+
+proc4 = subprocess.Popen(shlex.split("awk '{print $2}'"), stdin = proc3.stdout,
+                         stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+
+proc1.stdout.close() # Allow proc1 to receive a SIGPIPE if proc2 exits.
+proc2.stdout.close()
+proc3.stdout.close()
+
+out, err = proc4.communicate()
+proc4.stdout.close()
+if out != '':
+	pid = int(out)
+	os.kill(pid, signal.SIGKILL)
+	proc4.stdout.close()
+	# check if kill complete
+	proc1 = subprocess.Popen(shlex.split('ps aux'), stdout = subprocess.PIPE)
+	proc2 = subprocess.Popen(shlex.split('grep main.py'), stdin = proc1.stdout,
+	                         stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+	proc3 = subprocess.Popen(shlex.split('grep 8000'), stdin = proc2.stdout,
+	                         stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+	
+	proc4 = subprocess.Popen(shlex.split("awk '{print $2}'"), stdin = proc3.stdout,
+	                         stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+	
+	proc1.stdout.close() # Allow proc1 to receive a SIGPIPE if proc2 exits.
+	proc2.stdout.close()
+	proc3.stdout.close()
+	
+	out, err = proc4.communicate()
+	
+	if out == '':
+		print '== KILL SUCCESS =='
+	else:
+		print '== KILL FAILURE =='
+	proc4.stdout.close()
+else:
+	print "== PORT CLEAN RUNNING DIRECTLY =="
+
+os.system("nohup python main.py 8000 > running_pid.txt&")
