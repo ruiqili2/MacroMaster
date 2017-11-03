794ba136aad05f5d1a4707976b77b0e9805890d9
ruiqili2 <ruiqili2@illinois.edu>
2017-11-02 22:43:42 -0500

    fix route

--- MacroMaster/settings.py
+++ MacroMaster/settings.py
@@ -149,5 +149,5 @@ USE_TZ = True
 #https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-	"/home/ruiqili2/cs411-project/static",
+	"$HOME/cs411-project/static",
 )
--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,5 @@
 from django.conf.urls import url
-
 from . import views
-
-
 urlpatterns = [
     url(r'^index/', views.index, name='index'),
     url(r'^home/', views.home, name='home'),
