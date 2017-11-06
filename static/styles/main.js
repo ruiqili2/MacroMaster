f707b42b2fc1ad21958450c92e6dc62000009a8a
jrxk <xiajianf@gmail.com>
2017-11-05 21:23:25 -0600

    rating -> input

--- static/styles/main.js
+++ static/styles/main.js
@@ -3,8 +3,10 @@ console.log('hola')
 let ratingText = document.querySelector('#rating-user');
 let rating = document.querySelector('.star-ratings.mutable');
 let ratingTop = document.querySelector('.star-ratings.top.mutable');
-let latestRating = "0%";
-let realTimeRating = "0%";
+let latestRatingStr = "0%";
+let latestRating = 0; 
+let realTimeRatingStr = "0%";
+let realTimeRating = 0;
 rating.addEventListener("mousemove", ratingMoveHandler);
 rating.addEventListener("mouseout", ratingOutHandler);
 rating.addEventListener("click", ratingMouseUpHandler);
@@ -12,20 +14,23 @@ let ratingPosLeft = rating.offsetLeft;
 console.log("rating left:", ratingPosLeft);
 function ratingMoveHandler(e) {
     //console.log("mouse location:", e.clientX, e.clientY);
-    let ratingData = Math.round(((e.clientX - ratingPosLeft)/rating.clientWidth)*100);
-    realTimeRating = String(ratingData) + '%';
+    realTimeRating = Math.round(((e.clientX - ratingPosLeft)/rating.clientWidth)*100);
+    realTimeRatingStr = String(realTimeRating) + '%';
     console.log("rating data:", realTimeRating);
     // ratingData = ratingData / rating.width
-    ratingTop.style.width = realTimeRating;
-    ratingText.innerHTML = "My Rating: " + realTimeRating;
+    ratingTop.style.width = realTimeRatingStr;
+    // ratingText.innerHTML = "My Rating: " + realTimeRating;
+    ratingText.value = realTimeRating;
 }
 function ratingOutHandler() {
-    ratingTop.style.width = latestRating;
+    ratingTop.style.width = latestRatingStr;
     realTimeRating = latestRating;
-    ratingText.innerHTML = "My Rating: " + realTimeRating;
+    ratingText.value = latestRating;
+    // ratingText.innerHTML = "My Rating: " + realTimeRating;
 }
 function ratingMouseUpHandler() {
     latestRating = realTimeRating;
+    latestRatingStr = String(latestRating) + '%';
 }
 
 
