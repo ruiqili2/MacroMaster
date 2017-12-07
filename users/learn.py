a8bfa72185ce6002ec06de9899dab46b3222fd58
ruiqili2 <ruiqili2@illinois.edu>
2017-12-06 23:24:34 -0600

    pull

--- users/learn.py
+++ users/learn.py
@@ -33,6 +33,7 @@ def recommend_engine(favorites_recipes, time_tag, user):
             if not tag in w_diction:
                 w_diction[tag] = 1
             else:
+                w_diction[tag] += 1
     print("...created tags dictionary...")
     all_recipe = Recipes.objects.all()
     result_list = []
