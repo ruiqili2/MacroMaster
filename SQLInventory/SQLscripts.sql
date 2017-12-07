dfe4e7e2f6de627ad72590372d04c701d765b10f
zwang199 <zwang199@illinois.edu>
2017-12-06 22:26:57 -0600

    dsf

--- SQLInventory/SQLscripts.sql
+++ SQLInventory/SQLscripts.sql
@@ -365,9 +365,10 @@ CREATE PROCEDURE sp_updateRecipeDetail
     END;
 //
 
-ALTER TABLE Recipes ADD INDEX idx_recipes_cal (calories DESC)
-ALTER TABLE Recipes ADD INDEX idx_recipes_rat (rating DESC)
-ALTER TABLE Recipes ADD FULLTEXT ftxt_recipes_name (name)
+ALTER TABLE schema_recipes ADD INDEX idx_recipes_cal (calories DESC)
+ALTER TABLE schema_recipes ADD INDEX idx_recipes_rat (rating DESC)
+
+
 
 
 
